3c1d3d9352c5c0e5f2f524c046de53988db88ebd
Marek Vitula <mv.marekvitula@gmail.com>
2021-03-28 14:49:43 +0200

    CMAKE for tvOS

--- cmake/Toolchains/tvos.cmake
+++ cmake/Toolchains/tvos.cmake
@@ -12,17 +12,15 @@
 # PPSSPP platform flags
 set(MOBILE_DEVICE ON)
 set(USING_GLES2 ON)
-set(TVOS_DEPLOYMENT_TARGET 14.3)
+set(TVOS_DEPLOYMENT_TARGET 11.4)
 add_definitions(
   -DGL_ETC1_RGB8_OES=0
   -U__STRICT_ANSI__
 )
 
 set(OPENGL_LIBRARIES "-framework OpenGLES")
-# set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mios-version-min=${IPHONEOS_DEPLOYMENT_TARGET}")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mtvos-version-min=${TVOS_DEPLOYMENT_TARGET}")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
-# set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mios-version-min=${IPHONEOS_DEPLOYMENT_TARGET}")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mtvos-version-min=${TVOS_DEPLOYMENT_TARGET}")
 set(CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LIBRARY "libc++")
 
@@ -31,6 +29,7 @@ set(CMAKE_SYSTEM_NAME Darwin)
 set(CMAKE_SYSTEM_VERSION 1)
 set(CMAKE_SYSTEM_PROCESSOR arm64)
 set(IOS ON)
+set(TVOS ON)
 set(CMAKE_CROSSCOMPILING ON)
 set(CMAKE_MACOSX_BUNDLE YES)
 set(CMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_REQUIRED "NO")
@@ -38,18 +37,10 @@ set(CMAKE_XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY "")
 set(CMAKE_XCODE_ATTRIBUTE_TVOS_DEPLOYMENT_TARGET ${TVOS_DEPLOYMENT_TARGET})
 
 set(IOS_PLATFORM "TVOS")
-# Setup iOS platform unless specified manually with IOS_PLATFORM
-# if(NOT DEFINED IOS_PLATFORM)
-#   set(IOS_PLATFORM "TVOS")
-# endif()
 set(IOS_PLATFORM ${IOS_PLATFORM} CACHE STRING "Type of OS Platform")
 
 # Check the platform selection and setup for developer root
-if(IOS_PLATFORM STREQUAL "OS")
-  set(IOS_SDK_NAME "iphoneos")
-elseif(IOS_PLATFORM STREQUAL "SIMULATOR")
-  set(IOS_SDK_NAME "iphonesimulator")
-elseif(IOS_PLATFORM STREQUAL "TVOS")
+if(IOS_PLATFORM STREQUAL "TVOS")
   set(IOS_SDK_NAME "appletvos")
 elseif(IOS_PLATFORM STREQUAL "SIMULATOR_TVOS")
   set(IOS_SDK_NAME "appletvsimulator")
@@ -57,7 +48,7 @@ else()
   message (FATAL_ERROR "Unsupported IOS_PLATFORM value selected '${IOS_PLATFORM}'. Please choose OS or leave default")
 endif()
 
-# Setup iOS developer location unless specified manually with CMAKE_IOS_DEVELOPER_ROOT
+# Setup iOS developer location unless specified manually with CMAKE_TVOS_DEVELOPER_ROOT
 if(NOT CMAKE_TVOS_SDK_ROOT)
   execute_process(COMMAND xcrun --sdk ${IOS_SDK_NAME} --show-sdk-path
     OUTPUT_VARIABLE CMAKE_TVOS_SDK_ROOT
@@ -66,17 +57,13 @@ if(NOT CMAKE_TVOS_SDK_ROOT)
   message (STATUS "Toolchain using default tvOS SDK: ${CMAKE_TVOS_SDK_ROOT}")
 endif()
 
-set(CMAKE_TVOS_SDK_ROOT ${CMAKE_TVOS_SDK_ROOT} CACHE PATH "Location of the selected iOS SDK")
+set(CMAKE_TVOS_SDK_ROOT ${CMAKE_TVOS_SDK_ROOT} CACHE PATH "Location of the selected tvOS SDK")
 
 # Set the sysroot default to the most recent SDK
-set(CMAKE_OSX_SYSROOT ${CMAKE_TVOS_SDK_ROOT} CACHE PATH "Sysroot used for iOS support")
+set(CMAKE_OSX_SYSROOT ${CMAKE_TVOS_SDK_ROOT} CACHE PATH "Sysroot used for tvOS support")
 
-# set the architecture for iOS 
-if(IOS_PLATFORM STREQUAL "OS")
-  # When ffmpeg has been rebuilt for arm64 use:
-  set(IOS_ARCH "arm64;armv7")
-  #set(IOS_ARCH "armv7")
-elseif(IOS_PLATFORM STREQUAL "TVOS")
+# set the architecture for tvOS 
+if(IOS_PLATFORM STREQUAL "TVOS")
   set(IOS_ARCH "arm64")
 else()
   set(IOS_ARCH "i386;x86_64")
@@ -101,7 +88,7 @@ set(CMAKE_SYSTEM_FRAMEWORK_PATH
   ${CMAKE_TVOS_SDK_ROOT}/Developer/Library/Frameworks
 )
 
-# only search the iOS sdks, not the remainder of the host filesystem
+# only search the tvOS sdks, not the remainder of the host filesystem
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
